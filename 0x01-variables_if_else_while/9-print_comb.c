e0250263465052d27ad5369366903a20572c34fe
Tonyefelix23 <victorayole59@gmail.com>
2023-02-19 17:56:40 -0800

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,13 +4,19 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
     int i, j;
+    
     for (i = 0; i < 10; i++) {
-        printf("%d%s", i, (i == 9) ? "\n" : ", ");
-        for (j = i + 1; j < 10; j++) {
-            printf("%d%s", j, (j == 9) ? "\n" : ", ");
+        for (j = i; j < 10; j++) {
+            putchar(j + '0');
+            if (j < 9) {
+                putchar(',');
+                putchar(' ');
+            }
         }
+        putchar('\n');
     }
     return 0;
 }
