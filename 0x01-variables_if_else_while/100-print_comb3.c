7577bf80c800679f46ceb78fcc7fac565fc44c0c
Tonyefelix23 <victorayole59@gmail.com>
2023-02-19 18:49:44 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,19 +7,23 @@
 int main(void)
 {
 	int i, j;
+
 	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j< 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			if (i != 9 || j != 8)
+			if (i == 8 && j == 9)
+			{
+				putchar('\n');
+			}
+			else
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-	putchar('\n');
-	return 0;
+	return (0);
 }
